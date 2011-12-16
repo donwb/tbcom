9059849e280b6afeea52bfd0f82cbf32f0ebb25c
unknown <donwb@.(none)>
2011-12-15 22:30:17 -0500

    added a special page for amanda to see bashful

---
+++ amanda.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.IO;
+
+public partial class amanda : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        StringBuilder sb = new StringBuilder();
+        DirectoryInfo di = new DirectoryInfo(Server.MapPath("amanda"));
+        FileInfo[] files = di.GetFiles();
+
+
+        foreach (FileInfo fi in files)
+        {
+            sb.Append("<img src=\"amanda/" + fi.Name + "\" /> <br/>" + fi.Name + "<p/>");
+
+        }
+
+        myLiteral.Text = sb.ToString();
+    }
+}
