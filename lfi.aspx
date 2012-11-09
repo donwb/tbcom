0a338964fe6aebdc02e58158e3147056621ff0d5
Don Browning <don.browning@gmail.com>
2012-11-08 23:18:00 -0500

    added lfi shots

---
+++ lfi.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/main.master" AutoEventWireup="true" CodeFile="lfi.aspx.cs" Inherits="lfi" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <font color="white">
+        <asp:Literal runat="server" ID="myLiteral"></asp:Literal>
+    </font>
+
+</asp:Content>
+
---
+++ lfi.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+public partial class lfi : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        StringBuilder sb = new StringBuilder();
+        DirectoryInfo di = new DirectoryInfo(Server.MapPath("lfi"));
+        FileInfo[] files = di.GetFiles();
+
+
+        foreach (FileInfo fi in files)
+        {
+            sb.Append("<img src=\"lfi/" + fi.Name + "\" /> <br/>" + fi.Name + "<p/>");
+
+        }
+
+        myLiteral.Text = sb.ToString();
+    }
+}
